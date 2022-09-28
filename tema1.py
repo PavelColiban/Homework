fdf7996918027c343d2560744f47d328c8928c45
Paul <113927946+PavelColiban@users.noreply.github.com>
2022-09-28 17:48:10 +0300

    Add files via upload
    Tema pe acasa 28.09.22

---
+++ tema1.py
@@ -0,0 +1,7 @@
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time = %s" % now)
+print ("Today's date:  = %s/%s/%s" % (now.day, now.month, now.year))
+print ("The time is now: = %s:%s:%s" % (now.hour, now.minute, now.second))
+
+
