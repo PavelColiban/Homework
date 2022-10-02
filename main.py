46dfdecc39391530c58e4f51cb50c41fb864ea6c
Paul <113927946+PavelColiban@users.noreply.github.com>
2022-10-02 09:35:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# cifra = 27
+# varianta = 0
+# while cifra != varianta:
+#
+#     if varianta > cifra:
+#         print("Varianta e prea mare, mai incearca")
+#     else:
+#         print("Varianta e prea mica, mai incearca")
+#     varianta = int(input("Introduceti cifra "))
+#
+# print("Felicitari ati castigat")
+txt = "Life it seems to fade away"
+print("Ghici unul din cuvitele din prop: " + txt)
+word = "fade"
+varianta = str(input("Introduce'i cuvantul "))
+
+while word != varianta:
+    if(varianta == word ):
+        print("Ai ghicit !!")
+
+    else:
+        print("Incearca din nou !!")
+
+
+
+print("Ai ghicit !!")
+
