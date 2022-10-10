bded48e0617a96f1e3524d2398442ba7b73ff1a4
Paul <113927946+PavelColiban@users.noreply.github.com>
2022-10-10 15:41:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,67 @@
-# cifra = 27
-# varianta = 0
-# while cifra != varianta:
-#
-#     if varianta > cifra:
-#         print("Varianta e prea mare, mai incearca")
-#     else:
-#         print("Varianta e prea mica, mai incearca")
-#     varianta = int(input("Introduceti cifra "))
-#
-# print("Felicitari ati castigat")
-txt = "Life it seems to fade away"
-print("Ghici unul din cuvitele din prop: " + txt)
-word = "fade"
-varianta = str(input("Introduce'i cuvantul "))
+#Initial Values
+pers_name = input("Set the name ")
+pers_hp = 100
+max_pers_hp = 100
+armor = 50
+nr_hits = 1
 
-while word != varianta:
-    if(varianta == word ):
-        print("Ai ghicit !!")
 
+def speciall(pers_hp, dmg0 = 10):
+    global armor,  nr_hits
+
+    if nr_hits == 10:
+        armor = armor - dmg0
     else:
-        print("Incearca din nou !!")
+        pers_hp = pers_hp - dmg0
+    return pers_hp
 
 
 
-print("Ai ghicit !!")
 
+
+#Fucntion declaration
+def attack(pers_hp, dmg = 5):
+#calling a global variables
+    global armor, nr_hits
+
+#if we have armor
+    if armor - dmg > 0:
+# decrease armor level
+        armor = armor - dmg
+    else:
+# decrease hp level
+        pers_hp = pers_hp-dmg
+    return pers_hp # return the value
+
+def heal(pers_hp,live):
+# calling a global variable
+    global max_pers_hp
+
+# condition to not add more hp then max
+    if pers_hp + live > max_pers_hp:
+        pers_hp = max_pers_hp
+    else:
+        pers_hp = pers_hp + live
+
+    return pers_hp # return the value
+
+#Call Functions
+pers_hp = attack(pers_hp, 40)
+print(pers_hp)
+
+pers_hp = heal(pers_hp, 20)
+print(pers_hp)
+
+# Make a battle
+for i in range(1,100):
+#stop cycle if hp drop below 0
+    if pers_hp > 0:
+        pers_hp = attack(pers_hp)
+    else:
+        break
+
+print("Live ", pers_hp)
+print("Armor", armor)
+print("Hit nr ", i)
+
+print("You are dead")
